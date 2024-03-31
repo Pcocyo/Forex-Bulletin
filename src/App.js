f04a32818de89073420ee64ffb64c4fea0d5f2dd
elson <joshuaelson46@gmail.com>
2024-03-31 10:53:43 +0800

    tests concluded

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import DaughnutTest from './Tests/DaughnutTest'
+import Tests from './Tests/Tests'
+{/*  */}
+{/* <Tests/> */}
 const App = () => {
   return (
-    <div>App</div>
+    <DaughnutTest/>
   )
 }
 
---
+++ src/Tests/Tests.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import jsonData from './fmp-data (2).json';
+const Tests = () => {
+  console.log(jsonData.slice(1,6))
+  return (
+    <div>Tessts</div>
+  )
+}
+
+export default Tests
